a8a3f3df7c2c41e1f0f635d60be231d6bc916a65
niraj <nirajr1992@gmail.com>
2024-06-12 14:07:24 -0700

    Record 1 video at a time, move download code, update style

--- src/WebcamStreamCapture.jsx
+++ src/WebcamStreamCapture.jsx
@@ -13,9 +13,6 @@ const WebcamStreamCapture = () => {
     mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
       mimeType: "video/webm",
     });
-    // Check if recording in video / webm format is possible.
-    const isVideoFormatSupported = MediaRecorder.isTypeSupported("video/mp4");
-    console.log("isVideoFormatSupported", isVideoFormatSupported);
     mediaRecorderRef.current.addEventListener(
       "dataavailable",
       handleDataAvailable
@@ -26,7 +23,10 @@ const WebcamStreamCapture = () => {
   const handleDataAvailable = React.useCallback(
     ({ data }) => {
       if (data.size > 0) {
-        setRecordedChunks((prev) => prev.concat(data));
+        // To record all the frames
+        // setRecordedChunks((prev) => prev.concat(data));
+        // To record only the last frame
+        setRecordedChunks([data]);
       }
     },
     [setRecordedChunks]
@@ -37,32 +37,21 @@ const WebcamStreamCapture = () => {
     setCapturing(false);
   }, [mediaRecorderRef, webcamRef, setCapturing]);
 
-  const handleDownload = React.useCallback(async () => {
-    if (recordedChunks.length) {
-      const blob = new Blob(recordedChunks, {
-        type: "video/webm",
-      });
-      const url = URL.createObjectURL(blob);
-      // setData(url);
-      const a = document.createElement("a");
-      document.body.appendChild(a);
-      a.style = "display: none";
-      a.href = url;
-      a.download = "react-webcam-stream-capture.mp4";
-      a.click();
-      window.URL.revokeObjectURL(url);
-      //   setRecordedChunks([]);
-    }
-  }, [recordedChunks]);
-
   return (
     <>
       <section className="flex flex-col items-center justify-center">
-        <Webcam audio={false} mirrored={true} ref={webcamRef} />
+        <h2 className="text-2xl mb-2">Webcam</h2>
+        <Webcam
+          audio={false}
+          mirrored={true}
+          ref={webcamRef}
+          className="w-80"
+        />
         <div className="flex pt-2">
           <button
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 disabled:cursor-not-allowed disabled:opacity-75"
             onClick={handleStartCaptureClick}
+            disabled={capturing}
           >
             Start Capture
           </button>
@@ -75,17 +64,9 @@ const WebcamStreamCapture = () => {
           </button>
         </div>
       </section>
-      <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
+      <hr className="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700" />
       <section className="flex flex-col items-center justify-center">
         <Convert recordedChunks={recordedChunks} />
-        {recordedChunks.length > 0 && (
-          <button
-            className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-            onClick={handleDownload}
-          >
-            Download
-          </button>
-        )}
       </section>
     </>
   );
