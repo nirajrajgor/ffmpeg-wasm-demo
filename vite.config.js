dc4ca42896320b28d5970f6dd2c4d170cb438cb3
niraj <nirajr1992@gmail.com>
2024-06-12 16:23:12 -0700

    Update config to deploy to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/ffmpeg-webcam-demo/",
   optimizeDeps: {
     exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
   },
