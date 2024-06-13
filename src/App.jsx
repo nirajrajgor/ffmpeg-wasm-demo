970cf61cc0287c9aec5d60daf36fae12a82d4018
niraj <nirajr1992@gmail.com>
2024-06-13 14:58:23 -0700

    Update repo name

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <hr className="w-48 h-1 mx-auto my-1 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700" />
       <div className="text-center mb-4">
         <a
-          href="https://github.com/nirajrajgor/ffmpeg-webcam-demo"
+          href="https://github.com/nirajrajgor/ffmpeg-wasm-demo"
           target="_blank"
           rel="noreferrer"
           className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
