bef0a004bb3cf2654b3e6692f9d8767173d98c10
niraj <nirajr1992@gmail.com>
2024-06-13 12:47:09 -0700

    Add github link, update hr tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import WebcamStreamCapture from "./WebcamStreamCapture.jsx";
 
 function App() {
-  return <WebcamStreamCapture />;
+  return (
+    <>
+      <WebcamStreamCapture />
+      <hr className="w-48 h-1 mx-auto my-1 bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700" />
+      <div className="text-center mb-4">
+        <a
+          href="https://github.com/nirajrajgor/ffmpeg-webcam-demo"
+          target="_blank"
+          rel="noreferrer"
+          className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+        >
+          Github
+        </a>
+      </div>
+    </>
+  );
 }
 
 export default App;
